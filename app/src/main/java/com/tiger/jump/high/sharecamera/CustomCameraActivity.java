50e6527f530af13d2a42c5d6ca7f27181edbdc30
yangbo66558 <yangbo66558@126.com>
2016-03-24 14:35:54 +0800

    add some icon and ani for view

---
+++ app/src/main/java/com/tiger/jump/high/sharecamera/CustomCameraActivity.java
@@ -0,0 +1,15 @@
+package com.tiger.jump.high.sharecamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yb on 16-3-24.
+ */
+public class CustomCameraActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_camera);
+    }
+}
--- app/src/main/java/com/tiger/jump/high/sharecamera/MainActivity.java
+++ app/src/main/java/com/tiger/jump/high/sharecamera/MainActivity.java
@@ -1,13 +1,24 @@
 package com.tiger.jump.high.sharecamera;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    private Button btnCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iniView();
     }
+
+    private void iniView() {
+        btnCapture = (Button) findViewById(R.id.btn_capture);
+    }
+
+
 }
